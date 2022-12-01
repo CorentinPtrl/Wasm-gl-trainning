2b31712a656c4e8964a6c8bef0774305d7cc0878
Corentin PITREL <gomarco@outlook.fr>
2022-12-01 11:59:07 +0100

    add base of mvp

--- internal/wasm/triangle.go
+++ internal/wasm/triangle.go
@@ -3,19 +3,22 @@ package main
 import (
 	"Wasm-gl-trainning/internal/wasm/utils"
 	_ "Wasm-gl-trainning/internal/wasm/utils"
+	"github.com/go-gl/mathgl/mgl32"
 	webgl "github.com/seqsense/webgl-go"
 	"syscall/js"
+	"unsafe"
 )
 
 const vsSource = `
 attribute vec3 position;
-uniform vec3 offset;
+uniform mat4 Pmatrix;
+uniform mat4 Vmatrix;
+uniform mat4 Mmatrix;
 attribute vec3 color;
 varying vec3 vColor;
 
 void main(void) {
-  gl_Position = vec4(position, 1.0) + vec4(offset, 1.0);
-  //gl_Position.x = gl_Position.x + 1.0;
+  gl_Position = Pmatrix*Vmatrix*Mmatrix*vec4(position, 1.);
   vColor = color;
 }
 `
@@ -49,6 +52,9 @@ var deltaMouse [2]float64
 var uniform_fetched bool
 var cube_offset utils.Vector3f
 var offset webgl.Location
+var movMatrix mgl32.Mat4
+var tmark float32
+var rotation = float32(0)
 
 func setup() {
 	canvas := js.Global().Get("document").Call("getElementById", "glcanvas")
@@ -81,15 +87,16 @@ func setup() {
 	}
 }
 
-func render() {
+func render(now float32) {
 	width := gl.Canvas.ClientWidth()
 	height := gl.Canvas.ClientHeight()
-	gl.GetUniformLocation(program, "offset")
 	gl.UseProgram(program)
-	if uniform_fetched == false {
-		offset = gl.GetUniformLocation(program, "offset")
-		uniform_fetched = true
-	}
+
+	// Associate attributes to vertex shader
+	PositionMatrix := gl.GetUniformLocation(program, "Pmatrix")
+	ViewMatrix := gl.GetUniformLocation(program, "Vmatrix")
+	ModelMatrix := gl.GetUniformLocation(program, "Mmatrix")
+
 	gl.Uniform3fv(offset, cube_offset)
 	gl.BindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
 	position := gl.GetAttribLocation(program, "position")
@@ -105,12 +112,48 @@ func render() {
 	gl.Clear(gl.COLOR_BUFFER_BIT)
 	gl.Enable(gl.DEPTH_TEST)
 	gl.Viewport(0, 0, width, height)
+
+	ratio := float32(width / height)
+
+	// Generate and apply projection matrix
+	projMatrix := mgl32.Perspective(mgl32.DegToRad(45.0), ratio, 1, 100.0)
+	var projMatrixBuffer *[16]float32
+	projMatrixBuffer = (*[16]float32)(unsafe.Pointer(&projMatrix))
+	projMatrixParsed := utils.NewMatrixFromArr(*projMatrixBuffer)
+	gl.UniformMatrix4fv(PositionMatrix, false, projMatrixParsed)
+
+	// Generate and apply view matrix
+	viewMatrix := mgl32.LookAtV(mgl32.Vec3{3.0, 3.0, 3.0}, mgl32.Vec3{0.0, 0.0, 0.0}, mgl32.Vec3{0.0, 1.0, 0.0})
+	var viewMatrixBuffer *[16]float32
+	viewMatrixBuffer = (*[16]float32)(unsafe.Pointer(&viewMatrix))
+	viewMatrixParsed := utils.NewMatrixFromArr(*viewMatrixBuffer)
+	gl.UniformMatrix4fv(ViewMatrix, false, viewMatrixParsed)
+
+	tdiff := now - tmark
+	tmark = now
+	rotation = rotation + float32(tdiff)/500
+
+	// Do new model matrix calculations
+	movMatrix = mgl32.HomogRotate3DX(0.5 * rotation)
+	movMatrix = movMatrix.Mul4(mgl32.HomogRotate3DY(0.3 * rotation))
+	movMatrix = movMatrix.Mul4(mgl32.HomogRotate3DZ(0.2 * rotation))
+
+	// Convert model matrix to a JS TypedArray
+	var modelMatrixBuffer *[16]float32
+	modelMatrixBuffer = (*[16]float32)(unsafe.Pointer(&movMatrix))
+
+	// Apply the model matrix
+	modelMatrixParsed := utils.NewMatrixFromArr(*modelMatrixBuffer)
+	gl.UniformMatrix4fv(ModelMatrix, false, modelMatrixParsed)
+
 	gl.DrawArrays(gl.TRIANGLES, 0, len(vertices)/3)
 
 }
 
 func main() {
 	setup()
+	movMatrix = mgl32.Ident4()
+
 	cube_offset = utils.Vector3f{}
 	doc := js.Global().Get("document")
 	mouseMoveEvt := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
@@ -135,9 +178,9 @@ func main() {
 	var renderFrame js.Func
 
 	renderFrame = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		//now := args[0].Float()
+		now := args[0].Float()
 		//fmt.Println("Actual time ", now)
-		render()
+		render(float32(now))
 		js.Global().Call("requestAnimationFrame", renderFrame)
 		return nil
 	})
